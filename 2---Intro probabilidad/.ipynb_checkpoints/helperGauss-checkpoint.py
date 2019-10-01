9bc850c43d82ba6455793ee9d50f134038e3c1ca
gdejesus <guille17@gmail.com>
2019-10-01 10:11:58 -0300

    [+] Ejemplo de gauss
    Ejemplo completo de gauss

---
+++ 2---Intro probabilidad/.ipynb_checkpoints/helperGauss-checkpoint.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+from ipykernel import kernelapp as app
+import helperGauss as helper
+
+class GaussModel:
+    def __init__(self,e=1e-8):
+        self.epsilon=e
+
+    def fit(self,images,labels):
+        self.means = list()
+        self.stds = list()
+        self.models = list()
+        for cat in range(10):
+            self.means = np.mean(images[labels==cat])
+            self.stds = np.std(images[labels==cat])
+            self.models.append((self.means,self.stds))
+
+    def predict(self,testImages):
+        # .pdf funcion de densidad de probabilidad
+        predictions = []
+        for cat in range(10):
+            media = self.models[cat][0]
+            desvio = self.models[cat][1]+self.epsilon
+            Gaussian = norm(media,desvio)
+            predictions.append(np.log(Gaussian.pdf(testImages)).sum(axis=1))
+            #predictions = np.array(predictions)
+        return np.argmax(predictions, axis=0)
+        
+    def score(self,images,categories):
+        predictions = self.predict(images)
+        compare = np.equal(predictions,categories)
+        nonzero = np.count_nonzero(compare)
+        return nonzero/len(categories)
